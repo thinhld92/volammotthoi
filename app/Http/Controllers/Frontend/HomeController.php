f8445be0e7195c2455981e91e6c9ab593aa1edf9
Thinh Le <thinhld92@gmail.com>
2025-06-16 11:00:10 +0700

    update home

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
 {
     public function index(){
         {
-            if (getWebsiteConfig('go_home')) {
+            if (getWebsiteConfig('go_home') > 0) {
                 return redirect()->route('home');
             }
             $welcomeBanners = Banner::query()
